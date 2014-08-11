9b625eab5bf341e9c7df5dabeab8371f48d72d20
Chris Stokoe <cms93@cam.ac.uk>
2014-08-11 13:53:34 +0100

    Memory fix plus config change

--- config/base.js
+++ config/base.js
@@ -1,13 +1,13 @@
 var config = {}
 
 //Application settings and paths
-config.appDir = '/home/cudl/node/cudl-services2';
+config.appDir = '/usr/local/cudl-node-services';
 config.dataDir = '/mnt/cudl-data';
 config.cacheDir = '/mnt/cache';
 
 //MySQL settings for cudl database services 
 config.mysqlPool = 10;
-config.mysqlHost = 'found-dom01.lib.cam.ac.uk';
+config.mysqlHost = '10.0.0.5';
 config.mysqlUser = 'viewerdevuser';
 config.mysqlPass = 'resuvedreweiv';
 config.mysqlData = 'viewerdev';
