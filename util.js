b83a332a8a6133171a37ffacfa995dc5fd0d329a
Hal Blackburn <hwtb2@cam.ac.uk>
2014-11-17 16:52:24 +0000

    Restrict access to JSON metadata
    Domains under cudl.lib.cam.ac.uk are given full access to metadata.
    Requests from other domains (via CORS) are only allowed to access
    embeddable content.
    
    Currently only JSON metadata marks itself as not being embeddable.

---
+++ util.js
@@ -0,0 +1,16 @@
+var url = require("url");
+var regexpQuote = require("regexp-quote");
+
+var CUDL_HOST = "cudl.lib.cam.ac.uk";
+var CUDL_HOST_REGEX = new RegExp("(?:^|\\.)" + regexpQuote(CUDL_HOST) + "$")
+
+module.exports.isExternalCorsRequest = function isExternalCorsRequest(req) {
+    var origin = req.header("origin") || "";
+    if(!origin) { return false; }
+
+    var host = url.parse(origin).hostname;
+
+    // If we have an origin header and it's not cudl, then it's an external cors
+    // request.
+    return !CUDL_HOST_REGEX.test(host);
+}
