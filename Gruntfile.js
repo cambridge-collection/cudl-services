ba75f65cd857c7bac3ddcd71ecfdb91664834d87
Hal Blackburn <hwtb2@cam.ac.uk>
2015-06-05 13:18:02 +0100

    Add Gruntfile with jshint task

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: [
+                '*.js', 'routes/**/*.js', 'config/**/*.js', 'bin/www',
+                'test/**/*.js'
+            ]
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint']);
+
+};
