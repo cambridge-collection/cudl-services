20f472c5aa709f7b80923e72b8ed619f4e985cc8
Chris Stokoe <cms93@cam.ac.uk>
2015-08-03 15:45:56 +0100

    Added new route to wrap api auth around the Darwin XTF server

---
+++ routes/darwin.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var request = require('request');
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', 
+	/* passport.authenticate('localapikey', { session: false }), */
+	function(req, res) {
+    		var url = config.darwinXTF + req.url;
+		console.log(url);
+		req.pipe(request(url)).pipe(res);	
+	}
+});
+
+module.exports = router;
