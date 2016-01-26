88cc68aa0e69352588a631094c5132465624b7fa
Hal Blackburn <hwtb2@cam.ac.uk>
2016-01-26 16:25:02 +0000

    Add missing semicolons in config/default.js

--- config/default.js
+++ config/default.js
@@ -1,4 +1,4 @@
-var config = {}
+var config = {};
 
 //Application settings and paths
 config.appDir = '/home/cms93/cudl-services';
@@ -14,7 +14,7 @@ config.postUser = 'cudldev';
 config.postPass = 'anwqxJcONu1NjUl';
 config.postDatabase = 'viewerdev';
 
-config.users = {}
+config.users = {};
 config.users["1a1c31e8-0c04-11e4-b6a8-bf55e15d8c9d"] = { username: 'cudl-dev', password: 'secret', email: 'cudl-admin@lib.cam.ac.uk' };
 config.users["385784fa-0c04-11e4-b5cc-a3745fee5631"] = { username: 'cudl-live', password: 'secret', email: 'cudl-admin@lib.cam.ac.uk' };
 config.users["436b3396-0c04-11e4-b7ed-0308c2efa56c"] = { username: 'genizah', password: 'secret', email: 'genizah@lib.cam.ac.uk' };
