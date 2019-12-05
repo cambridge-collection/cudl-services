d9ef943807d6d0f74a0eba7804a3b7dd35d828bd
Hal Blackburn <hwtb2@cam.ac.uk>
2019-12-05 12:02:29 +0000

    Shutdown server gracefully
    On Ctrl+C or SIGTERM the server will stop accepting connections, and
    the process will exit as soon as the current requests are handled.
    
    The db connections also get closed.

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,66 @@
 import Debugger from 'debug';
 import util from 'util';
+import { App } from './app';
+import { Config, loadConfigFromEnvar } from './config';
+import { using } from './resources';
+
 const debug = Debugger('cudl-services');
 
-import { getAppForConfig } from './app';
-import {Config, loadConfigFromEnvar} from './config';
+async function run() {
+  let config: Config;
+  try {
+    config = loadConfigFromEnvar();
+  } catch (e) {
+    console.error(`Error: ${e.message}`);
+    process.exit(1);
+  }
 
-let config: Config;
-try {
-  config = loadConfigFromEnvar();
+  if ((config && 'user' in config) || 'group' in config) {
+    console.error(`\
+  Error: config.user and config.group are no longer supported. Remove them from the \
+  config and start this process with the desired user.`);
+    process.exit(1);
+  }
+  if (process.getuid && process.getuid() === 0) {
+    console.error('Error: Running as root is not permitted');
+    process.exit(1);
+  }
+
+  await using(App.fromConfig(config), async application => {
+    application.expressApp.set('port', process.env.PORT || 3000);
+
+    const server = application.expressApp.listen(
+      application.expressApp.get('port'),
+      () => {
+        const msg = `Express server listening on ${util.inspect(
+          server?.address()
+        )}`;
+        (debug.enabled ? debug : console.log)(msg);
+      }
+    );
+
+    await new Promise((resolve, reject) => {
+      function shutdown() {
+        process.stdout.write(` Shutting down gracefully... `);
+        server.close(err => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      }
+
+      process.once('SIGINT', shutdown);
+      process.once('SIGTERM', shutdown);
+      server.once('error', reject);
+      server.once('close', resolve);
+    });
+  });
+  console.log(`server stopped.`);
 }
-catch (e) {
-  console.error(`Error: ${e.message}`);
+
+run().catch(e => {
+  console.error(`Error: Server exited with an uncaught exception:\n\n`, e);
   process.exit(1);
-}
-
-if ((config && 'user' in config) || 'group' in config) {
-  console.error(`\
-Error: config.user and config.group are no longer supported. Remove them from the \
-config and start this process with the desired user.`);
-  process.exit(1);
-}
-if (process.getuid && process.getuid() === 0) {
-  console.error('Error: Running as root is not permitted');
-  process.exit(1);
-}
-
-const app = getAppForConfig(config);
-app.set('port', process.env.PORT || 3000);
-
-const server = app.listen(app.get('port'), () => {
-  const msg = `Express server listening on ${util.inspect(server?.address())}`;
-  (debug.enabled ? debug : console.log)(msg);
 });
