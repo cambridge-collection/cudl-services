2d04e8c615f20e3cd0034fc3f31d63e704de959a
Hal Blackburn <hwtb2@cam.ac.uk>
2020-01-20 09:09:01 +0000

    Add key-based sorting function - sorted()

--- src/util.ts
+++ src/util.ts
@@ -77,3 +77,104 @@ export function requireRequestParams<T extends string>(
   }
   return obj;
 }
+
+export function factory<A, B>(
+  constructor: new (...args: A[]) => B
+): (...args: A[]) => B {
+  return (...args: A[]) => new constructor(...args);
+}
+
+/** The type of the only argument of a 1-ary constructor. */
+export type UnaryConstructorArg<
+  T extends new (arg: unknown) => unknown
+> = ConstructorParameters<T>[0];
+
+export type ComparePrimitive = string | number | boolean | ComparePrimitive[];
+export type CompareModifier = [Direction, CompareValue];
+export type CompareValue = ComparePrimitive | CompareModifier | CompareValue[];
+
+export function compare(a: CompareValue, b: CompareValue): number {
+  if (typeof a !== typeof b) {
+    throw new Error(
+      `compare() not supported between ${typeof a} and ${typeof b}`
+    );
+  }
+
+  if (!Array.isArray(a)) {
+    return a === b ? 0 : a < b ? -1 : 1;
+  }
+
+  // Directional compare modifier
+  if ((a.length > 0 && a[0] === asc) || a[0] === desc) {
+    b = b as CompareModifier | CompareValue[];
+    if (a.length !== 2) {
+      throw new Error(`Invalid compare direction modifier: modifiers must be a pair of [direction, value], \
+got: ${util.inspect(a)}`);
+    }
+    if (b.length !== 2 || b[0] !== a[0]) {
+      throw new Error(
+        `Invalid compare direction modifier: modifiers on both comparison values must use the same direction`
+      );
+    }
+
+    return compare(a[1], b[1]) * (a[0] === desc ? -1 : 1);
+  }
+
+  a = a as CompareValue[];
+  b = b as CompareValue[];
+
+  for (let i = 0; i < Math.min(a.length, b.length); ++i) {
+    const order = compare(a[i], b[i]);
+    if (order !== 0) {
+      return order;
+    }
+  }
+  // shorter array is less
+  return a.length - b.length;
+}
+
+const asc: unique symbol = Symbol('compare.asc');
+const desc: unique symbol = Symbol('compare.desc');
+compare.asc = asc;
+compare.desc = desc;
+type Direction = typeof asc | typeof desc;
+
+export function sorted<T extends ComparePrimitive>(items: Iterable<T>): T[];
+export function sorted<T>(
+  items: Iterable<T>,
+  key: (value: T) => CompareValue
+): T[];
+export function sorted<T>(
+  items: Iterable<T>,
+  key?: (value: T) => CompareValue
+): T[] {
+  if (key === undefined) {
+    const sorted = Array.from(
+      (items as Iterable<unknown>) as Iterable<ComparePrimitive>
+    );
+    sorted.sort((a, b) => compare(a, b));
+    return (sorted as unknown[]) as T[];
+  }
+
+  const indexedItems = index(items);
+  const keys = indexedItems.map(([_, value]) => key(value));
+  indexedItems.sort(([ia], [ib]) => compare(keys[ia], keys[ib]));
+
+  // Remove indexes in place
+  const sorted = (indexedItems as unknown) as T[];
+  for (let i = 0; i < indexedItems.length; ++i) {
+    sorted[i] = indexedItems[i][1];
+  }
+
+  return sorted;
+}
+
+function index<T>(items: Iterable<T>): Array<[number, T]> {
+  const index: Array<[number, T]> = [];
+  let i = 0;
+  for (const item of items) {
+    index[i] = [i, item];
+    i++;
+  }
+  return index;
+}
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,10 @@
-import { isEnumMember } from '../src/util';
+import {
+  compare,
+  ComparePrimitive,
+  CompareValue,
+  isEnumMember,
+  sorted,
+} from '../src/util';
 
 test('isEnumMember', () => {
   expect.assertions(1);
@@ -17,4 +23,153 @@ test('isEnumMember', () => {
   if (isEnumMember(Foo, val)) {
     blah(val);
   }
+});
+
+describe('sorting', () => {
+  describe('compare()', () => {
+    test('string', () => {
+      expect(compare('a', 'a')).toBe(0);
+      expect(compare('a', 'b')).toBe(-1);
+      expect(compare('b', 'a')).toBe(1);
+    });
+
+    test('number', () => {
+      expect(compare(1, 1)).toBe(0);
+      expect(compare(1, 2)).toBe(-1);
+      expect(compare(2, 1)).toBe(1);
+    });
+
+    test('boolean', () => {
+      expect(compare(true, true)).toBe(0);
+      expect(compare(false, false)).toBe(0);
+      expect(compare(true, false)).toBe(1);
+      expect(compare(false, true)).toBe(-1);
+    });
+
+    test('array', () => {
+      expect(compare([], [])).toBe(0);
+      expect(compare([1], [1])).toBe(0);
+      expect(compare(['a'], ['a'])).toBe(0);
+      expect(compare(['a'], ['b'])).toBe(-1);
+      expect(compare(['b'], ['a'])).toBe(1);
+      expect(compare([1], [1])).toBe(0);
+      expect(compare([0], [1])).toBe(-1);
+      expect(compare([1], [0])).toBe(1);
+
+      expect(compare(['a', 'b'], ['a', 'c'])).toBe(-1);
+      expect(compare(['a', 'c'], ['a', 'b'])).toBe(1);
+      expect(compare(['a', 'c'], ['a', 'c'])).toBe(0);
+
+      expect(compare(['a', ['b']], ['a', ['c']])).toBe(-1);
+      expect(compare(['a', ['c']], ['a', ['b']])).toBe(1);
+      expect(compare(['a', ['c']], ['a', ['c']])).toBe(0);
+      expect(compare(['a', ['c', 'b']], ['a', ['c', 'd']])).toBe(-1);
+      expect(compare(['a', ['c', 'd']], ['a', ['c', 'b']])).toBe(1);
+      expect(compare(['a', ['c', 'b']], ['a', ['c', 'b']])).toBe(0);
+
+      // When arrays of differing lengths are compared, the shorter array is less
+      // to break ties
+      expect(compare([1, 2], [1, 2, 3])).toBe(-1);
+      expect(compare([1, 2, 3], [1, 2])).toBe(1);
+    });
+
+    test.each<[CompareValue, CompareValue, number]>([
+      [[compare.desc, 1], [compare.desc, 1], 0],
+      [[compare.desc, 0], [compare.desc, 1], 1],
+      [[compare.desc, 1], [compare.desc, 0], -1],
+
+      [
+        [compare.desc, [0, [compare.desc, 'b']]],
+        [compare.desc, [0, [compare.desc, 'a']]],
+        1,
+      ],
+      [
+        [compare.desc, [0, [compare.desc, 'a']]],
+        [compare.desc, [0, [compare.desc, 'b']]],
+        -1,
+      ],
+    ])('directional - compare(%s, %s) === %d', (a, b, expected) => {
+      expect(normaliseNegativeZero(compare(a, b))).toBe(expected);
+    });
+  });
+
+  function normaliseNegativeZero(n: number) {
+    return Object.is(-0, n) ? 0 : n;
+  }
+
+  describe('sorted()', () => {
+    test('new array is returned', () => {
+      const input = [3, 1, 2];
+      const result = sorted(input);
+      expect(result).not.toBe(input);
+      expect(result).toEqual([1, 2, 3]);
+    });
+
+    test('key function is invoked once per element', () => {
+      const key = jest.fn(x => x);
+      const input = [3, 1, 2];
+      expect(sorted(Array.from(input), key)).toEqual([1, 2, 3]);
+      expect(key.mock.calls.map(call => call[0])).toEqual(input);
+    });
+
+    test.each([
+      [[], []],
+      [[0], [0]],
+      [
+        [0, 1],
+        [0, 1],
+      ],
+      [
+        [1, 0],
+        [0, 1],
+      ],
+    ])('sorted(%j) returns %j', (unsorted, expected) => {
+      expect(sorted(unsorted)).toEqual(expected);
+    });
+
+    test.each([
+      [[], []],
+      [[0], [0]],
+      [
+        [0, 1],
+        [1, 0],
+      ],
+      [
+        [1, 0],
+        [1, 0],
+      ],
+    ])("sorted(%j, 'desc') returns %j", (unsorted, expected) => {
+      expect(sorted(unsorted, x => [compare.desc, x])).toEqual(expected);
+    });
+
+    test.each<[Array<{ thing: number }>, Array<{ thing: number }>]>([
+      [[], []],
+      [[{ thing: 0 }], [{ thing: 0 }]],
+      [
+        [{ thing: 0 }, { thing: 1 }],
+        [{ thing: 0 }, { thing: 1 }],
+      ],
+      [
+        [{ thing: 1 }, { thing: 0 }],
+        [{ thing: 0 }, { thing: 1 }],
+      ],
+    ])("sorted(%j, *key*, 'desc') returns %j", (unsorted, expected) => {
+      expect(sorted(unsorted, t => t.thing)).toEqual(expected);
+    });
+
+    test.each<[Array<{ thing: number }>, Array<{ thing: number }>]>([
+      [[], []],
+      [[{ thing: 0 }], [{ thing: 0 }]],
+      [
+        [{ thing: 0 }, { thing: 1 }],
+        [{ thing: 1 }, { thing: 0 }],
+      ],
+      [
+        [{ thing: 1 }, { thing: 0 }],
+        [{ thing: 1 }, { thing: 0 }],
+      ],
+    ])("sorted(%j, *key*,s 'desc') returns %j", (unsorted, expected) => {
+      expect(sorted(unsorted, t => [compare.desc, t.thing])).toEqual(expected);
+    });
+  });
 });
