c625cee91d4961e3be5711c77628fcf5c3598123
Hal Blackburn <hwtb2@cam.ac.uk>
2021-01-06 09:42:30 +0000

    fix(docker): container was not terminating after SIGINT
    Using Ctrl+C with a `docker container run ...` command was not shutting
    down the node service because sh was not propagating SIGINT to node.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 # Generate a config file under /etc/cudl-services from environment variables
 confd -onetime -backend env || { echo "Error: Templating config files with environment variables failed" >&2 && exit 1; }
 # Execute the app as the node user
-su-exec node "$@"
+exec su-exec node "$@"
