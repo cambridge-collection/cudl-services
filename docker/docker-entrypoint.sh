50c87e567e9c7c0259cffcb60cffe185d8c4cfd3
Hal Blackburn <hwtb2@cam.ac.uk>
2021-03-19 16:49:35 +0000

    feat(Docker): provide a default value for NODE_CONFIG_MODULE

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 # Generate a config file under /etc/cudl-services from environment variables
 confd -onetime -backend env || { echo "Error: Templating config files with environment variables failed" >&2 && exit 1; }
+
+export NODE_CONFIG_MODULE=${NODE_CONFIG_MODULE:-cudl-services/lib/cudl-config}
+
 # Execute the app as the node user
 exec su-exec node "$@"
