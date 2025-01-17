9ffa492ff707925b8d098bb7ecdcec0d52970eef
jlf44 <jlf44@cam.ac.uk>
2025-01-17 16:20:52 +0000

    Update config for production

--- src/cudl-config.ts
+++ src/cudl-config.ts
@@ -45,7 +45,7 @@ const partialConfigValidator = requireNotUndefined(
 );
 
 const DEFAULT_ZACYNTHIUS_SERVICE_URL =
-  'http://sandbox-codex-zacynthius-transcription.s3-website.eu-west-2.amazonaws.com/';
+  'http://cul-cudl-codex-zacynthius-transcription.s3-website.eu-west-1.amazonaws.com/';
 
 export function splitEnvarPaths(paths: string | undefined) {
   return (paths || '').split(':').filter(p => !!p);
